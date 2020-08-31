b89098a682e36fb3d277b6d40df98839a2b92180
sicg18 <37753311+sicg18@users.noreply.github.com>
2020-08-31 16:36:44 -0500

    Lector

--- src/controlador/Lector.java
+++ src/controlador/Lector.java
@@ -2,23 +2,46 @@ package controlador;
 
 import java.util.concurrent.Callable;
 
-public class Lector implements Callable<Double>{
+public class Lector implements Callable<Double> {
 
 	/*
 	 * Clase que hará la función de sumArray
 	 */
-	
-	public Lector () {
+
+	private double result;
+	private double[] numbers;
+
+	public Lector() {
 	}
-	
-	public Lector (String[] lista) {
-		
+
+	public Lector(String[] lista) {
+
+	}
+
+	private double sum(double array[]) {
+		double sum = 0;
+
+		for (double num : array) {
+			sum += num;
+		}
+
+		try {
+			Thread.sleep((long) (Math.random() * 1000));
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		return sum;
 	}
 
 	@Override
 	public Double call() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		System.out.println("sumando....");
+		return sum(numbers);
 	}
-	
+
+	public double getResult() {
+		return result;
+	}
+
 }
